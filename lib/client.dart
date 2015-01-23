3fe681acb449b2f3d5433a3aa7e8323bd56b6a52
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2015-01-23 20:38:53 +0100

    circle can be moved using the mouse

--- lib/client.dart
+++ lib/client.dart
@@ -14,7 +14,10 @@ class Game extends GameBase {
   Game() : super.noAssets('zfx_action_6', 'canvas', 800, 600);
 
   void createEntities() {
-    addEntity([new Circle(20.0), new Position(400.0, 300.0), new Color('#ffffff', '#ffffff')]);
+    TagManager tm = world.getManager(TagManager);
+    var e = addEntity([new Circle(20.0), new Position(400.0, 300.0), new Color('#ffffff', '#ffffff')]);
+    tm.register(e, playerTag);
+
     addEntity([new Triangle(20.0, 0.0), new Position(500.0, 300.0), new Color('#ffffff')]);
     addEntity([new Triangle(20.0, PI / 2), new Position(400.0, 200.0), new Color('#ffffff')]);
     addEntity([new Triangle(20.0, PI), new Position(300.0, 300.0), new Color('#ffffff')]);
@@ -22,6 +25,8 @@ class Game extends GameBase {
 
   List<EntitySystem> getSystems() {
     return [
+            new MouseInputHandlingSystem(canvas),
+
             new TweeningSystem(),
             new CanvasCleaningSystem(canvas, fillStyle: 'black'),
             new TriangleRenderingSystem(ctx),
@@ -30,5 +35,9 @@ class Game extends GameBase {
             new FpsRenderingSystem(ctx),
             new AnalyticsSystem(AnalyticsSystem.GITHUB, 'zfx_action_6')
     ];
+  }
+
+  onInit() {
+    world.addManager(new TagManager());
   }
 }
--- lib/src/client/systems/events.dart
+++ lib/src/client/systems/events.dart
@@ -1,3 +1,30 @@
 part of client;
 
 
+class MouseInputHandlingSystem extends VoidEntitySystem {
+  Mapper<Position> pm;
+  TagManager tm;
+  Point<int> offset = new Point<int>(0, 0);
+
+  CanvasElement canvas;
+
+  MouseInputHandlingSystem(this.canvas);
+
+  @override
+  void initialize() {
+    canvas.onMouseMove.listen((event) {
+      offset = event.offset;
+    });
+  }
+
+
+  @override
+  void processSystem() {
+    var e = tm.getEntity(playerTag);
+
+    var p = pm[e];
+
+    p.x = offset.x.toDouble();
+    p.y = offset.y.toDouble();
+  }
+}
