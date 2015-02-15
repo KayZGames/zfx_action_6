74c6c940d4a4b1e339c0ba6bbeec7285db17fda3
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2015-02-15 17:18:10 +0100

    overlay webgl canvas with context2d canvas for gamestate etc positions of messages are obviously off

--- lib/client.dart
+++ lib/client.dart
@@ -6,14 +6,14 @@ import 'dart:typed_data';
 import 'dart:web_gl';
 
 import 'package:zfx_action_6/shared.dart';
-import 'package:gamedev_helpers/gamedev_helpers.dart' hide Triangle, CanvasCleaningSystem;
+import 'package:gamedev_helpers/gamedev_helpers.dart' hide Triangle;
 import 'package:lawndart/lawndart.dart';
 
 //part 'src/client/systems/name.dart';
 part 'src/client/systems/highscore.dart';
 part 'src/client/systems/audio.dart';
 part 'src/client/systems/events.dart';
-//part 'src/client/systems/rendering.dart';
+part 'src/client/systems/rendering.dart';
 part 'src/client/systems/webgl.dart';
 
 class Game extends GameBase {
@@ -22,8 +22,15 @@ class Game extends GameBase {
 
   AnalyserNode audioAnalyser;
 
-  Game() : super.noAssets('zfx_action_6', 'canvas', 800, 600, webgl: true) {
+  CanvasElement hudCanvas;
+  CanvasRenderingContext2D hudCtx;
+
+  Game() : super.noAssets('zfx_action_6', '#game', 800, 600, webgl: true) {
     Tween.waypointsLimit = 1;
+    hudCanvas = querySelector('#hud');
+    hudCtx = hudCanvas.context2D;
+    hudCtx..textBaseline = 'top'
+          ..font = '12px Verdana';
   }
 
   void createEntities() {
@@ -48,7 +55,7 @@ class Game extends GameBase {
 
         new GameStateUpdateingSystem(),
 
-        new MouseInputHandlingSystem(canvas),
+        new MouseInputHandlingSystem(hudCanvas),
         new AcccelerationSystem(),
         new ThrusterParticleEmittingSystem(),
         new MovementSystem(),
@@ -62,15 +69,15 @@ class Game extends GameBase {
 
         new WebGlCanvasCleaningSystem(ctx),
 
-//        new CanvasCleaningSystem(canvas, fillStyle: 'black'),
-//        new RageModeRenderer(ctx),
+        new CanvasCleaningSystem(hudCanvas, fillStyle: 'rgba(0, 0, 0, 0)'),
+        new RageModeRenderer(hudCtx),
         new BackgroundDotRenderingSystem(ctx),
         new ParticleRenderingSystem(ctx),
         new TriangleRenderingSystem(ctx),
         new CircleRenderingSystem(ctx),
-//        new MessageRenderingSystem(ctx),
-//        new GameOverRenderingSystem(ctx),
-//        new GameStateRenderingSystem(ctx),
+        new MessageRenderingSystem(hudCtx),
+        new GameOverRenderingSystem(hudCtx),
+        new GameStateRenderingSystem(hudCtx),
         new PainometerRenderingSystem(ctx),
 //        new FpsRenderingSystem(ctx),
 
@@ -103,7 +110,8 @@ class Game extends GameBase {
   }
 
   onInitDone() {
-    querySelector('canvas').style.cursor = 'none';
+    querySelector('#game').style.cursor = 'none';
+    querySelector('#hud').style.cursor = 'none';
     querySelector('#reasonForAudio').style.display = 'none';
     eventBus.fire(new AnalyticsTrackEvent('audio', '${mediaStreamSource != null}'));
   }
