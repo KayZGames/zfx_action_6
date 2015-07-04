0a49d8f5f7515c6242b204bb0ddf1c8658a9151a
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2015-07-04 19:51:15 +0200

    removed analytics

--- lib/client.dart
+++ lib/client.dart
@@ -17,7 +17,6 @@ part 'src/client/systems/rendering.dart';
 part 'src/client/systems/webgl.dart';
 
 class Game extends GameBase {
-
   MediaStreamAudioSourceNode mediaStreamSource;
 
   AnalyserNode audioAnalyser;
@@ -29,8 +28,9 @@ class Game extends GameBase {
     Tween.waypointsLimit = 1;
     hudCanvas = querySelector('#hud');
     hudCtx = hudCanvas.context2D;
-    hudCtx..textBaseline = 'top'
-          ..font = '12px Verdana';
+    hudCtx
+      ..textBaseline = 'top'
+      ..font = '12px Verdana';
   }
 
   void createEntities() {
@@ -38,9 +38,9 @@ class Game extends GameBase {
   }
 
   Map<int, List<EntitySystem>> getSystems() {
-    return {0: [
+    return {
+      GameBase.rendering: [
         new WebGlCanvasCleaningSystem(ctx),
-
         new CanvasCleaningSystem(hudCanvas, fillStyle: 'rgba(0, 0, 0, 0)'),
         new RageModeRenderer(hudCtx),
         new ParticleRenderingSystem(ctx),
@@ -52,14 +52,12 @@ class Game extends GameBase {
         new GameStateRenderingSystem(hudCtx),
         new PainometerRenderingSystem(ctx),
 //        new FpsRenderingSystem(ctx),],
-        ],
-      1: [
+      ],
+      GameBase.physics: [
         new CameraSwitchingSystem(),
         new CircleDestructionSystem(),
         new PainAnalysingSystem(audioAnalyser),
-
         new GameStateUpdateingSystem(),
-
         new MouseInputHandlingSystem(hudCanvas),
         new AcccelerationSystem(),
         new ThrusterParticleEmittingSystem(),
@@ -69,20 +67,16 @@ class Game extends GameBase {
         new CircleCollisionSystem(),
         new HealthColoringSystem(),
         new FriendCollectingSystem(),
-
         new TweeningSystem(),
-
         new TriangleSpawningSystem(),
         new BackgroundDotSpawner(),
         new FriendSpawner(),
         new LifetimeSystem(),
         new AttentionDelayDecreasingSystem(),
-
         new CalmDownSystem(),
-
         new HighScoreSystem(),
-
-        new AnalyticsSystem(AnalyticsSystem.GITHUB, 'zfx_action_6')]};
+      ]
+    };
   }
 
   onInit() {
@@ -91,39 +85,38 @@ class Game extends GameBase {
 
     var audioCtx = new AudioContext();
 
-    return window.navigator.getUserMedia(audio: true, video: false).then((mediaStream) {
+    return window.navigator
+        .getUserMedia(audio: true, video: false)
+        .then((mediaStream) {
       audioAnalyser = audioCtx.createAnalyser();
       audioAnalyser.fftSize = fftSize;
       mediaStreamSource = audioCtx.createMediaStreamSource(mediaStream);
       mediaStreamSource.connectNode(audioAnalyser);
-    }).catchError((error) {
-    });
+    }).catchError((error) {});
   }
 
   onInitDone() {
     querySelector('#game').style.cursor = 'none';
     querySelector('#hud').style.cursor = 'none';
     querySelector('#reasonForAudio').style.display = 'none';
-    eventBus.fire(new AnalyticsTrackEvent('audio', '${mediaStreamSource != null}'));
+    eventBus
+        .fire(new AnalyticsTrackEvent('audio', '${mediaStreamSource != null}'));
   }
 }
-
 
 void createInitialEntities(World world) {
   TagManager tm = world.getManager(TagManager);
   GroupManager gm = world.getManager(GroupManager);
-  var e = world.createAndAddEntity(
-      [
-          new Circle(20.0),
-          new Position(400.0, 300.0, 0.0),
-          new Color(red: 1.0, green: 1.0, blue: 1.0),
-          new Heartbeat(),
-          new Health(100.0, 100.0)]);
+  var e = world.createAndAddEntity([
+    new Circle(20.0),
+    new Position(400.0, 300.0, 0.0),
+    new Color(red: 1.0, green: 1.0, blue: 1.0),
+    new Heartbeat(),
+    new Health(100.0, 100.0)
+  ]);
   tm.register(e, playerTag);
   gm.add(e, circleGroup);
 
-  e = world.createAndAddEntity([
-      new Camera()
-    ]);
+  e = world.createAndAddEntity([new Camera()]);
   tm.register(e, cameraTag);
 }
